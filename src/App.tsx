d7a21f8a156bfa95e609a821480a496fb71783bc
Paul Haddou <32875065+PaulHaddou@users.noreply.github.com>
2026-02-19 10:29:44 +0100

    feat: setup basic front ui (#12)

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import QubicBridgeLogomark from "./components/ui/assets/qubic-bridge-logomark";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <div className="mx-auto max-w-[1280px] p-8 text-center">
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img
-            src={viteLogo}
-            className="h-24 p-6 transition-[filter] duration-300 will-change-[filter] hover:drop-shadow-[0_0_2em_#646cffaa]"
-            alt="Vite logo"
-          />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img
-            src={reactLogo}
-            className="h-24 p-6 transition-[filter] duration-300 will-change-[filter] hover:drop-shadow-[0_0_2em_#61dafbaa] animate-[logo-spin_20s_linear_infinite] motion-reduce:animate-none"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="p-8">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-[#888]">Click on the Vite and React logos to learn more</p>
+    <div className="w-screen p-8 text-center flex justify-center items-center h-screen">
+      <QubicBridgeLogomark />
     </div>
   );
 }
-
-export default App;
