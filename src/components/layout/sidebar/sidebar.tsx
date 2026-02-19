023f35cf4d3e5562471ad4362e11f4271ea3a65a
Paul Haddou <github@paul-haddou.com>
2026-02-19 12:45:20 +0100

    feat: sidebar components added

--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -37,7 +37,6 @@ export default function Sidebar() {
           isFullWidth
         />
       </div>
-
       <div className="flex w-full h-fit bg-primary">
         <StatusSection />
       </div>
