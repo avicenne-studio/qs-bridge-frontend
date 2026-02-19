67612d1d222b31c70b40336fb0dfe3e835cf2852
Paul Haddou <github@paul-haddou.com>
2026-02-19 18:09:42 +0100

    feat: wallet area components addition

--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -37,6 +37,7 @@ export default function Sidebar() {
           isFullWidth
         />
       </div>
+
       <div className="flex w-full h-fit bg-primary">
         <StatusSection />
       </div>
