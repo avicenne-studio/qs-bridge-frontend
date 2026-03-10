d39ca860e69e75ca000a64dfd103f49304fb0006
Paul Haddou <github@paul-haddou.com>
2026-03-10 11:20:59 +0100

    feat: no wallet connected cases handled

--- src/domains/history/react/history.page.tsx
+++ src/domains/history/react/history.page.tsx
@@ -1,8 +1,11 @@
 import ComboTextStats from "@/components/stats/combo-text-stats";
 import EmptyHistory from "@/domains/history/react/components/empty-history";
+import NoWalletConnectedPanel from "@/components/no-wallet-connected-panel";
 import HistoryFilters from "@/domains/history/react/components/filters/history-filters";
 import HistoryTable from "@/domains/history/react/components/table/history-table";
 import { MOCKED_HISTORY_DATA } from "@/domains/history/history.constants";
+import useSolanaWallet from "@/hooks/useSolanaWallet";
+import { useQubicWallet } from "@/providers/QubicWalletProvider";
 import cn from "@/utils/classnames";
 import { Lock, Network } from "lucide-react";
 import type { ComponentProps } from "react";
@@ -10,6 +13,9 @@ import type { ComponentProps } from "react";
 type Stats = ComponentProps<typeof ComboTextStats>;
 
 export default function HistoryPage() {
+  const solana = useSolanaWallet();
+  const qubic = useQubicWallet();
+
   // Todo: get stats from backend
   const orders = MOCKED_HISTORY_DATA;
 
@@ -20,8 +26,17 @@ export default function HistoryPage() {
 
   const hasOrders = orders.length > 0;
 
+  if (!solana.connected && !qubic.connected) {
+    return (
+      <NoWalletConnectedPanel
+        label="No wallet connected"
+        description="Connect a wallet to view your transaction history."
+      />
+    );
+  }
+
   return (
-    <div className="w-full pt-6 flex gap-10 flex-col">
+    <div className="w-full pt-6 flex gap-10 flex-col h-full">
       <section className={cn("grid w-full gap-3", "grid-cols-1 md:grid-cols-4")}>
         {stats.map(({ title, value, currency, Icon }) => (
           <ComboTextStats key={title} title={title} value={value} currency={currency} Icon={Icon} />
@@ -29,7 +44,6 @@ export default function HistoryPage() {
       </section>
 
       <HistoryFilters />
-
       {hasOrders ? <HistoryTable data={orders} /> : <EmptyHistory />}
     </div>
   );
